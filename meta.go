22c24763f63f866b26c31a1c2d14ac0ae170e225
Daan Gerits <daan.gerits@gmail.com>
2024-04-19 19:15:54 +0200

    unpacking the envelope
    Signed-off-by: Daan Gerits <daan.gerits@gmail.com>

--- meta.go
+++ meta.go
@@ -1,12 +1,17 @@
 package mini
 
+type MetaEnvelope struct {
+    Path string
+    Data Meta
+}
+
 type Meta struct {
-	Client      string
-	Environment string
-	Id          string
-	Name        string
-	Kind        string
-	Description string
-	Version     string
-	Config      string
+    Client      string
+    Environment string
+    Id          string
+    Name        string
+    Kind        string
+    Description string
+    Version     string
+    Config      string
 }
